7f13d530e25e0cc2f92e9dabe50caf4daccdcd42
Farid AMOUSSA <amousssa.farid2@gmail.com>
2021-07-05 14:37:47 +0100

    Fix db:migrate

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Sample2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Porto Novo'
+    config.time_zone = 'Paris'
     config.active_record.default_timezone = :local
   end
 end
